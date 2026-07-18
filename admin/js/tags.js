26decfe7d1daee7ca7d0b4c234e183dac29892ed
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-18 05:28:00 -0300

    Update tags.js

--- admin/js/tags.js
+++ admin/js/tags.js
@@ -1,67 +1,9 @@
-<main class="content container-fluid p-4">
+document.addEventListener("DOMContentLoaded", carregarTags);
 
-    <div class="d-flex justify-content-between align-items-center mb-4">
+async function carregarTags(){
 
-        <h2>TAGs</h2>
+    const resposta = await apiGet(ACTION.LISTAR_TAGS);
 
-        <button id="btnNovaTag" class="btn btn-primary">
+    console.log(resposta);
 
-            <i class="bi bi-plus-circle"></i>
-
-            Nova TAG
-
-        </button>
-
-    </div>
-
-    <div class="card p-3">
-
-        <div class="mb-3">
-
-            <input
-
-                id="pesquisa"
-
-                class="form-control"
-
-                placeholder="Pesquisar TAG...">
-
-        </div>
-
-        <div class="table-responsive">
-
-            <table class="table table-hover align-middle">
-
-                <thead>
-
-                    <tr>
-
-                        <th>ID</th>
-
-                        <th>TOKEN</th>
-
-                        <th>STATUS</th>
-
-                        <th>PET</th>
-
-                        <th width="220">AÇÕES</th>
-
-                    </tr>
-
-                </thead>
-
-                <tbody id="listaTags">
-
-                </tbody>
-
-            </table>
-
-        </div>
-
-    </div>
-
-</main>
-
-<script src="js/config.js"></script>
-<script src="js/api.js"></script>
-<script src="js/tags.js"></script>
+}
