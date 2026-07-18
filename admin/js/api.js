66f3fd621b8b56ac01d5e7ff9aef40ab2f334470
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-18 08:21:05 -0300

    Update api.js

--- admin/js/api.js
+++ admin/js/api.js
@@ -1,42 +1,262 @@
 /*************************************************
- * PET NFC ADMIN
+ * PET NFC
  * api.js
+ * Versão 1.0.0
  *************************************************/
 
+/**
+ * Requisição GET
+ */
 async function apiGet(action, params = {}) {
 
-    const url = new URL(CONFIG.API_URL);
+    try {
 
-    url.searchParams.set("action", action);
+        const url = new URL(CONFIG.API_URL);
 
-    Object.keys(params).forEach(chave => {
+        url.searchParams.append("action", action);
 
-        url.searchParams.set(chave, params[chave]);
+        Object.keys(params).forEach(key => {
+            url.searchParams.append(key, params[key]);
+        });
 
-    });
+        const response = await fetch(url.toString(), {
+            method: "GET"
+        });
 
-    const resposta = await fetch(url);
+        return await response.json();
 
-    return await resposta.json();
+    } catch (erro) {
+
+        console.error(erro);
+
+        return {
+            sucesso: false,
+            mensagem: "Erro ao conectar com o servidor."
+        };
+
+    }
 
 }
 
-async function apiPost(obj) {
 
-    const resposta = await fetch(CONFIG.API_URL, {
+/**
+ * Requisição POST
+ */
+async function apiPost(dados = {}) {
 
-        method: "POST",
+    try {
 
-        headers: {
+        const response = await fetch(CONFIG.API_URL, {
 
-            "Content-Type":"application/json"
+            method: "POST",
 
-        },
+            headers: {
+                "Content-Type": "application/json"
+            },
 
-        body: JSON.stringify(obj)
+            body: JSON.stringify(dados)
+
+        });
+
+        return await response.json();
+
+    } catch (erro) {
+
+        console.error(erro);
+
+        return {
+            sucesso: false,
+            mensagem: "Erro ao conectar com o servidor."
+        };
+
+    }
+
+}
+
+
+/*************************************************
+ * PET
+ *************************************************/
+
+async function buscarPet(token) {
+
+    return await apiGet(
+
+        ACTION.BUSCAR_PET,
+
+        {
+            token: token
+        }
+
+    );
+
+}
+
+
+async function cadastrarPet(dados) {
+
+    dados.action = ACTION.CADASTRAR_PET;
+
+    return await apiPost(dados);
+
+}
+
+
+async function editarPet(dados) {
+
+    dados.action = ACTION.EDITAR_PET;
+
+    return await apiPost(dados);
+
+}
+
+
+async function atualizarFoto(token, foto) {
+
+    return await apiPost({
+
+        action: ACTION.ATUALIZAR_FOTO,
+
+        token: token,
+
+        foto: foto
 
     });
 
-    return await resposta.json();
+}
+
+
+async function enviarLocalizacao(token, latitude, longitude) {
+
+    return await apiPost({
+
+        action: ACTION.LOCALIZACAO,
+
+        token: token,
+
+        latitude: latitude,
+
+        longitude: longitude
+
+    });
+
+}
+
+
+/*************************************************
+ * ADMIN
+ *************************************************/
+
+async function gerarTag() {
+
+    return await apiGet(
+
+        ACTION.GERAR_TAG
+
+    );
+
+}
+
+
+async function gerarLote(qtd) {
+
+    return await apiGet(
+
+        ACTION.GERAR_LOTE,
+
+        {
+            qtd: qtd
+        }
+
+    );
+
+}
+
+
+async function listarTags() {
+
+    return await apiGet(
+
+        ACTION.LISTAR_TAGS
+
+    );
+
+}
+
+
+async function buscarTag(token) {
+
+    return await apiGet(
+
+        ACTION.BUSCAR_TAG,
+
+        {
+            token: token
+        }
+
+    );
+
+}
+
+
+async function bloquearTag(token) {
+
+    return await apiPost({
+
+        action: ACTION.BLOQUEAR_TAG,
+
+        token: token
+
+    });
+
+}
+
+
+async function reativarTag(token) {
+
+    return await apiPost({
+
+        action: ACTION.REATIVAR_TAG,
+
+        token: token
+
+    });
+
+}
+
+
+async function resetarTag(token) {
+
+    return await apiPost({
+
+        action: ACTION.RESETAR_TAG,
+
+        token: token
+
+    });
+
+}
+
+
+async function excluirTag(token) {
+
+    return await apiPost({
+
+        action: ACTION.EXCLUIR_TAG,
+
+        token: token
+
+    });
+
+}
+
+
+async function estatisticas() {
+
+    return await apiGet(
+
+        ACTION.ESTATISTICAS
+
+    );
 
 }
