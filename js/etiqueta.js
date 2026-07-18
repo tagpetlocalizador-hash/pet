b7ef38aec6c64763c2d0de696576b91cad7ade4b
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-18 06:58:30 -0300

    Update etiqueta.js

--- js/etiqueta.js
+++ js/etiqueta.js
@@ -1,1 +1,81 @@
+/*************************************************
+ * LINKA GIFT
+ * etiqueta.js
+ *************************************************/
 
+document.addEventListener("DOMContentLoaded", iniciar);
+
+async function iniciar() {
+
+    // Token recebido na URL
+    const params = new URLSearchParams(window.location.search);
+
+    const token = params.get("token");
+
+    if (!token) {
+
+        alert("Token não informado.");
+
+        return;
+
+    }
+
+    // Busca os dados da TAG
+
+    const resposta = await apiGet(
+
+        ACTION.BUSCAR_TAG,
+
+        {
+
+            token: token
+
+        }
+
+    );
+
+    if (!resposta.sucesso) {
+
+        alert(resposta.mensagem);
+
+        return;
+
+    }
+
+    // Monta a URL pública
+
+    const url =
+
+        "https://tagpetlocalizador-hash.github.io/pet/?token="
+
+        + token;
+
+    // TOKEN
+
+    document.getElementById("token").innerHTML = token;
+
+    // LINK
+
+    document.getElementById("url").innerHTML = url;
+
+    // QR CODE
+
+    new QRCode(
+
+        document.getElementById("qrcode"),
+
+        {
+
+            text: url,
+
+            width: 230,
+
+            height: 230,
+
+            correctLevel: QRCode.CorrectLevel.H
+
+        }
+
+    );
+
+}
