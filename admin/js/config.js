f3b572c66521ca52b54894d69f162d9b1fc5b2a5
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-18 05:20:07 -0300

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,1 +1,32 @@
+/*************************************************
+ * PET NFC ADMIN
+ * config.js
+ *************************************************/
 
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycbyUaF8EHlWZcgL0T3alf438MPNpeHgPm_dLqSrwiUGvIj4ggDrDcr3vSOCflCg8aVvQ/exec"
+
+};
+
+const ACTION = {
+
+    ESTATISTICAS: "estatisticas",
+
+    LISTAR_TAGS: "listarTags",
+
+    BUSCAR_TAG: "buscarTag",
+
+    GERAR_TAG: "gerarTag",
+
+    GERAR_LOTE: "gerarLote",
+
+    BLOQUEAR_TAG: "bloquearTag",
+
+    REATIVAR_TAG: "reativarTag",
+
+    RESETAR_TAG: "resetarTag",
+
+    EXCLUIR_TAG: "excluirTag"
+
+};
