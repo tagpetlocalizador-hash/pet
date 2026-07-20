d7446e5c91fd7d4134c62ab9914de9a4898247bc
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-19 23:19:10 -0300

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// ==============================
+// PET NFC
+// Configurações Globais
+// ==============================
+
+const CONFIG = {
+
+    API_URL: "COLE_AQUI_A_URL_DO_SEU_APPS_SCRIPT"
+
+};
