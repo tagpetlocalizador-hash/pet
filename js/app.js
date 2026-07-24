cf86ca7e59f7a923c88c3e5835c142e7c95bbf68
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-23 22:42:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -363,6 +363,15 @@ function mostrarPerfil() {
 
     }
 
+    setTimeout(function () {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "instant"
+    });
+
+}, 50);
 }
 
 
