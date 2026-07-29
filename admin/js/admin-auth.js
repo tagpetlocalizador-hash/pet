46fe65a7550db894c947862ba3364ca7c69f4565
tagpetlocalizador-hash <tagpet.localizador@gmail.com>
2026-07-28 21:26:24 -0300

    Update admin-auth.js

--- admin/js/admin-auth.js
+++ admin/js/admin-auth.js
@@ -1,1 +1,46 @@
+/*************************************************
+ * PET NFC ADMIN
+ * admin-auth.js
+ *************************************************/
 
+async function protegerPaginaAdmin() {
+
+    const tokenAdmin =
+        sessionStorage.getItem(
+            "pet_nfc_admin_token"
+        );
+
+
+    if (!tokenAdmin) {
+
+        window.location.href =
+            "login.html";
+
+        return;
+
+    }
+
+
+    const resposta =
+        await validarLoginAdmin(
+            tokenAdmin
+        );
+
+
+    if (!resposta.sucesso) {
+
+        sessionStorage.removeItem(
+            "pet_nfc_admin_token"
+        );
+
+        window.location.href =
+            "login.html";
+
+        return;
+
+    }
+
+}
+
+
+protegerPaginaAdmin();
